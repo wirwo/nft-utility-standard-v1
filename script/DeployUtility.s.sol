d70d8b44e548d4dee36391e23c19461eae964204
Waupelani <prawira.pikanto@gmail.com>
2023-07-25 00:16:33 -0400

    forge test

--- script/DeployUtility.s.sol
+++ script/DeployUtility.s.sol
@@ -7,7 +7,7 @@ import {NFTUtilities} from "../src/utility.sol";
 contract DeployUtility is Script {
     function run() external returns (NFTUtilities) {
         vm.startBroadcast();
-        NFTUtilities utility = new NFTUtilities();
+        NFTUtilities utility = new NFTUtilities(0x73A4b3b1a6C5C883ecD796D0dd5eD4f4e1E78d2b);
         vm.stopBroadcast();
         return utility;
     }
